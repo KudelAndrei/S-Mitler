1ce67eafd5d2641d535688a260cf1eaadd23f886
kudelandrei <kudel1995@mail.ru>
2017-03-24 17:02:52 +0300

    add services

--- app/js/common.js
+++ app/js/common.js
@@ -1,11 +1,35 @@
 $(document).ready(function() {
 
 	/* меню */
-	$("#my-menu").mmenu({});
+	$("#my-menu").mmenu({
+		extensions: ['effect-menu-slide', 'pagedim-black'],
+		navbar: {
+			title: '<img src="img/logo.svg" alt="Салон красоты в Минске">',
+		},
+		offCanvas: {
+			position: 'right',
+		}
+	});
+
+	/* гамбургег */
+	var api = $('#my-menu').data('mmenu');
+	api.bind('open', function(){
+		console.log('sef');
+	});
+
+	$('.hamburger').click(function(){
+		$(this).addClass('is-active');
+	});
+
+	$('#mm-blocker, .my-menu__item').click(function(){
+		$('.hamburger').removeClass('is-active');
+		console.log('awd');
+	});
+
 
 	/* слайдер */
 	$('.owl-services').owlCarousel({
-		items: 1,
+		items: 3,
 	});
 	
 });
