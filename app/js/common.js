059217fc4966335976e5518da18ad2607f7e194b
Kudel <kudel1995@mail.ru>
2017-03-26 20:32:48 +0300

    add partners

--- app/js/common.js
+++ app/js/common.js
@@ -30,6 +30,8 @@ $(document).ready(function() {
 	/* слайдер сервиса*/
 	$('.owl-services').owlCarousel({
 		items: 3,
+		dots: false,
+		nav: false,
 		responsive : {
 			0 : {
 				items: 1,
@@ -54,9 +56,42 @@ $(document).ready(function() {
 			owl.trigger('prev.owl.carousel');
 	})
 
+	/* карусель отзывов */
 	$('.owl-reviews').owlCarousel({
 		items: 1,
 		dots: true,
 	});
+
+	/* карусель клиентов */
+	$('.owl-partners').owlCarousel({
+		items: 4,
+		nav: false,
+		dots: true,
+		responsive : {
+			0 : {
+				items: 1,
+			},
+			500 : {
+				items: 2,
+			},
+			800: {
+				items: 3,
+			},
+			1250: {
+				items: 4,
+			},
+			1920: {
+				items: 6
+			}
+		}
+	});
+	var owl2 = $('.owl-partners');
+	owl2.owlCarousel();
+	$('.partners__next').click(function() {
+			owl2.trigger('next.owl.carousel');
+	})
+	$('.partners__prev').click(function() {
+			owl2.trigger('prev.owl.carousel');
+	})
 	
 });
